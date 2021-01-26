bbec11900732e9b2f81da3c4b248bc4e979f2cb6
r-veiga <roberto.aprende.mucho@gmail.com>
2021-01-26 15:01:49 +0100

    Subscribe to Observables in the html template (pipe async)

--- learn-angular-components2/src/app/timer/timer.service.ts
+++ learn-angular-components2/src/app/timer/timer.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from "@angular/core";
-import { Subject } from "rxjs";
+import { BehaviorSubject, Subject } from "rxjs";
 
 @Injectable()
 export class TimerService {
 
     private countdownTimerRef: any = null;
-    public countdown: number = 0;
     public paused: boolean = true;
     private init: number = 0;
     private countdownEndSource = new Subject<void>();
+    public countdownSource = new BehaviorSubject<number>(0);
     public countdownEnd$ = this.countdownEndSource.asObservable();
+    public countdown$ = this.countdownSource.asObservable();
 
     constructor() { }
 
@@ -24,7 +25,7 @@ export class TimerService {
         if (this.init && this.init > 0) {
             this.paused = true;
             this.clearTimeout();
-            this.countdown = this.init;
+            this.countdownSource.next(this.init);
         }
     }
 
@@ -40,13 +41,13 @@ export class TimerService {
 
     private doCountdown() {
         this.countdownTimerRef = setTimeout(() => {
-            this.countdown = this.countdown - 1;
+            this.countdownSource.next(this.countdownSource.getValue() - 1);
             this.processCountdown();
         }, 1000);
     }
 
     private processCountdown() {
-        if (this.countdown == 0) {
+        if (this.countdownSource.getValue() == 0) {
             this.countdownEndSource.next();
         }
         else {
@@ -60,5 +61,5 @@ export class TimerService {
             this.countdownTimerRef = null;
         }
     }
-
+ 
 }
