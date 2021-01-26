d04d135320b86d74f207e10af11f6465091f6588
r-veiga <roberto.aprende.mucho@gmail.com>
2021-01-26 07:44:53 +0100

    Set countdown initial value in UI by user
    Create a text box to introduce the countdown initial value in app.html

--- learn-angular-components1/src/app/app.module.ts
+++ learn-angular-components1/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
@@ -12,7 +13,8 @@ import { CountdownComponent } from './countdown/countdown.component';
     ProgressBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
