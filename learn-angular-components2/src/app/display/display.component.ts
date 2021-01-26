01f2e0cf81d60836515d8acc3d0f791a29cae7a8
r-veiga <roberto.aprende.mucho@gmail.com>
2021-01-26 10:52:30 +0100

    Change display from Seconds to MM:SS
    Regressive countdown takes the format "Minutes : Seconds" for display

--- learn-angular-components2/src/app/display/display.component.ts
+++ learn-angular-components2/src/app/display/display.component.ts
@@ -1,13 +1,26 @@
-import { Component, Input, Output, OnInit} from '@angular/core';
+import { Component, Input, Output, OnChanges, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-display',
   templateUrl: 'display.component.html',
   styleUrls: ['./display.component.scss']
 })
-export class DisplayComponent{
+export class DisplayComponent implements OnChanges{
 
   @Input() time:number = null;
+
+  private minutes:string = "00";
+  private seconds:string = "00";
+
   constructor() { }
 
+  ngOnChanges(changes) {
+    if(changes.time){
+      const minutes = Math.trunc(changes.time.currentValue / 60);
+      const seconds = changes.time.currentValue - minutes*60;
+
+      this.minutes = ("0" + minutes).substr(-2);
+      this.seconds = ("0" + seconds).substr(-2);
+    }
+  }
 }
